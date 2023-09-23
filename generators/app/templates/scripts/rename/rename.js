bb00281d25e497598bfae53d87248b866821903f
Wave <www.dandelion.ru@gmail.com>
2023-09-23 17:48:29 +0300

    fix(scripts): fix rename script

--- generators/app/templates/scripts/rename/rename.js
+++ generators/app/templates/scripts/rename/rename.js
@@ -1,6 +1,6 @@
 import { rename } from 'node:fs';
 
-rename('../../git.template', '.gitignore', (err) => {
+rename('git.template', '.gitignore', (err) => {
   if (err) throw err;
   console.log('Rename complete!');
 });
